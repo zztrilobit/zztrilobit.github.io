cb393e462150758cde7ccc4c201ed27a2d14bf64
zztrilobit <vbas1972@narod.ru>
2014-03-28 17:18:16 +0400

    2014-03-27-1

---
+++ quadro.js
@@ -0,0 +1,18 @@
+(function() {
+  var Quadro;
+
+  Quadro = (function() {
+    function Quadro() {}
+
+    return Quadro;
+
+  })();
+
+  ({
+    learn_click: function() {},
+    game_click: function() {},
+    next_click: function() {},
+    check_click: function() {}
+  });
+
+}).call(this);
